6bf026a6eb0e18854e151d207ebfdff1903ae1f7
Mikhail <rosseevm@gmail.com>
2021-10-04 13:33:04 +0300

    сделано меню, программа переделана в человеческом виде (в плане передачи данных по ссылке), переделана функция repair, сделано добавление id для трубы и кс

--- lab1.cpp
+++ lab1.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <fstream>
 using namespace std;
 
 struct pipe     //труба
@@ -7,7 +8,6 @@ struct pipe     //труба
 	int id, diametr;
 	double length;
 	bool in_repair;
-	string status_pipe, in_repair_str;
 };
 
 struct comp_st     // компрессорная станция
@@ -17,43 +17,93 @@ struct comp_st     // компрессорная станция
 	double efficency;
 };
 
-//задаю глобально тк используется в других функциях помимо main
-pipe my_pipe;
-comp_st c_st;
 
-
-pipe func_repair()
+void vvod_pipe(pipe &my_pipe)
 {
-	if (my_pipe.in_repair_str == "Да" || my_pipe.in_repair_str == "ДА" || my_pipe.in_repair_str == "да" || my_pipe.in_repair_str == "дА") 
-	{my_pipe.status_pipe="Труба находится в ремонте.";}
-	else if (my_pipe.in_repair_str == "Нет" || my_pipe.in_repair_str == "НЕТ" || my_pipe.in_repair_str == "нет" || my_pipe.in_repair_str == "нЕт" || my_pipe.in_repair_str == "неТ" || my_pipe.in_repair_str == "НЕт" 
-			|| my_pipe.in_repair_str == "нЕТ" || my_pipe.in_repair_str == "НеТ") {my_pipe.status_pipe="Труба находится в рабочем состоянии.";}
-	cout<<my_pipe.status_pipe<<endl;
-	return my_pipe;
+cout<<"Введите длину трубы"<<endl;
+cin>>my_pipe.length;
+cout<<"Введите диаметр трубы"<<endl;
+cin>>my_pipe.diametr;
+
 }
 
-pipe vivod_pipe ()
+void vvod_KS(comp_st &c_st)
+{
+cout<<"Введите название КС"<<endl;
+cin>>c_st.ks_name;
+cout<<"Введите количество цехов"<<endl;
+cin>>c_st.ceh_amount;
+cout<<"Введите количество работающих цехов"<<endl;
+cin>>c_st.ceh_INwork_amount;
+}
+/*
+void func_repair(pipe &my_pipe)
+{
+	cout<<(my_pipe.in_repair?"да":"нет");
+	//cout<<my_pipe.status_pipe<<endl;
+	//return my_pipe;
+}
+
+void change_repair(pipe &my_pipe)
+{
+
+//if (my_pipe.in_repair==0) my_pipe.status_pipe="Труба находится в рабочем состоянии.";
+//else my_pipe.status_pipe="Труба находится в нерабочем состоянии.";
+//return my_pipe;
+}
+*/
+
+//pipe add_pipe()
+//{
+//	my_pipe.id+=1;
+//	return my_pipe;
+//}
+
+void vivod_pipe (pipe &my_pipe)
+{
+cout << "Данные, собранные по указанной пользователем трубе:" <<endl
+	<< "Идентификатор трубы: " << my_pipe.id << endl
+    << "Длина данной трубы: " << my_pipe.length << ' ' << "метров" << endl
+	<< "Диаметр данной трубы: " << my_pipe.diametr << ' ' << "миллиметров" <<endl
+   	<< "Статус трубы: "<<(my_pipe.in_repair?"В ремонте":"Не в ремонте")<<endl;
+	//func_repair();
+
+//return my_pipe;
+}
+
+//pipe add_pipe()
+//{
+//	my_pipe.id+=1;
+//	return my_pipe;
+//}
+/*
+void vivod_pipe (pipe &my_pipe)
 {
 cout << "Данные, собранные по указанной пользователем трубе:" <<endl
 	<< "Идентификатор трубы: " << my_pipe.id << endl
     << "Длина данной трубы: " << my_pipe.length << ' ' << "метров" << endl
 	<< "Диаметр данной трубы: " << my_pipe.diametr << ' ' << "миллиметров" <<endl
    	<< "Статус трубы: ";
-	func_repair();//  <<endl;
+	func_repair();
 
-return my_pipe;
+//return my_pipe;
 }
+*/
+//comp_st add_KS()
+//{
+//c_st.id+=1;
+//return c_st;
+//}
 
-
-comp_st KS_efficency()
+void KS_efficency(comp_st &c_st)
 {
 	c_st.efficency=(c_st.ceh_INwork_amount*100)/c_st.ceh_amount;
 	cout<<c_st.efficency<<" %"<<endl;
-	return c_st;
+	//return c_st;
 }
 
 
-comp_st vivod_KS()
+void vivod_KS(comp_st &c_st)
 {
 cout << endl << "Данные, собранные по указанной пользователем КС" << endl
 	 << "Идентификатор КС: " << c_st.id << endl
@@ -61,36 +111,127 @@ cout << endl << "Данные, собранные по указанной пол
 	 << "Количество цехов на данной КС: " << c_st.ceh_amount <<endl
 	 << "Количество работающих цехов на данный момент: " << c_st.ceh_INwork_amount << endl
 	 << "Эффективность КС: ";
-	 KS_efficency();
-return c_st;
+	 KS_efficency(c_st);
+//return c_st;
+}
+/*
+void save_pipe(pipe &my_pipe)
+{
+ofstream out;
+out.open("/home/mikhail/vim/semester3/laboratory-works/info_lab1.txt");
+if(out.is_open())
+	{
+		out << vivod_pipe(my_pipe);
+	}
+	out.close();
+
 }
 
-int main()
+void save_KS(comp_st &c_st)
 {
+ofstream out;
+out.open("/home/mikhail/vim/semester3/laboratory-works/info_lab1.txt");
+if(out.is_open())
+	{
+		out << vivod_KS();
+	}
+	out.close();
+
+}
+*/
+void menu(pipe &my_pipe,comp_st &c_st)
+{
+	int x;
+	while (true)
+	{
+		cout<< "1. Добавить трубу" << endl 
+			<< "2. Добавить КС" << endl
+			<< "3. Просмотр всех объектов" << endl
+			<< "4. Редактировать трубу" << endl
+			<< "5. Редактировать КС" << endl
+			<< "6. Сохранить" << endl
+			<< "7. Загрузить" << endl
+			<< "0. Выход" << endl;
+	cin>>x;
+	switch (x) {
+	case 1:
+			if(my_pipe.id<1)
+				++my_pipe.id;
+			cout<<"Труба добавлена!";
+		break;
+	case 2:
+			if(c_st.id<1)
+				++c_st.id;
+			cout<<"КС добавлена!";
+		break;
+	case 3:
+		vivod_pipe(my_pipe);
+		vivod_KS(c_st);
+		break;
+	case 4:
+	cout<<"пока так";
+			//change_repair();
+		break;
+	case 5:
+			cout<<"КС отредактирована!";
+		break;
+	case 6:
+		cout<<"Yt pyf.!";
+		break;
+	case 7:
+			//save_pipe(my_pipe);
+			//save_KS(c_st);
+			cout<<"тоже так";
+		break;
+	case 0:
+		cout<<"exit";
+		break;
+	default:
+		cout<<"Ошибка при вводе!"<<endl;
+		break;
+	}
+	}
+}	
+
+
+int main()
+{	
+	pipe my_pipe;
+	comp_st c_st;
+
+	//задекларировали в мэйне параметры трубы
 	my_pipe.id=0, my_pipe.length=0.00, my_pipe.in_repair=0, my_pipe.diametr=0;
+	//задекларировали в мэйне параметры кс
+	c_st.id=0, c_st.ceh_amount=0, c_st.ceh_INwork_amount=0, c_st.efficency=0.00;
 
-	c_st.id=1, c_st.ceh_amount=0, c_st.ceh_INwork_amount=0, c_st.efficency=0.00;
+	
+	vvod_pipe(my_pipe);
+	//труба ввод на консоль
+	//cout<<"Прошу ввести длину трубы"<<endl;
+	//cin>>my_pipe.length;
+	//cout<<"Прошу ввести диаметр трубы"<<endl;
+	//cin>>my_pipe.diametr;
+	vvod_KS(c_st);
+	menu(my_pipe,c_st);
+	//кс ввод на консоль
+	//cout<<"Введите название КС"<<endl;
+	//cin>>c_st.ks_name;
+	//cout<<"Введите количество цехов"<<endl;
+	//cin>>c_st.ceh_amount;
+	//cout<<"Введите количество работающих цехов"<<endl;
+	//cin>>c_st.ceh_INwork_amount;
 
+	//запись в файл
+	//ofstream out;
+	//out.open("/home/mikhail/vim/semester3/laboratory-works/info_lab1.txt");
+	//if(out.is_open())
+	//{
+	//	out << vivod_pipe();
+	//	out << vivod_KS();
+	//}
+	//out.close();
+	//cout << endl << "Данные были записаны в файл!";
 
-	//труба ввод
-	cout<<"Прошу ввести длину трубы"<<endl;
-	cin>>my_pipe.length;
-	cout<<"Прошу ввести диаметр трубы"<<endl;
-	cin>>my_pipe.diametr;
-	cout <<"Находится ли труба в состоянии ремонта"<< endl;	
-	cin>>my_pipe.in_repair_str;
-
-	//кс ввод
-	cout<<"Введите название КС"<<endl;
-	cin>>c_st.ks_name;
-	cout<<"Введите количество цехов"<<endl;
-	cin>>c_st.ceh_amount;
-	cout<<"Введите количество работающих цехов"<<endl;
-	cin>>c_st.ceh_INwork_amount;
-
-	//вывод данных
-	vivod_pipe();
-	vivod_KS();
 	return 0;
 
 }
